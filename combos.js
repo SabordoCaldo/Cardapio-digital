c65f6378bb30a134c404d7ca2c119a1110cff109
Sabor do caldo <sabordocaldo.passosmg@gmail.com>
2026-04-02 23:10:48 -0300

    Create combos.js

---
+++ combos.js
@@ -0,0 +1,16 @@
+// ==========================
+// COMBOS
+// ==========================
+const COMBOS = [
+
+    {
+        id: 1001,
+        nome: "Combo Caldo + Refrigerante Lata",
+        descricao: "1 caldo + 1 refrigerante lata 350ml",
+        preco: 27.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "combo",
+        disponivel: true
+    }
+
+];
