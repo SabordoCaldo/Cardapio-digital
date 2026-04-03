8d3557ca24e0d71950f99f646770662012dcf2dc
Sabor do caldo <sabordocaldo.passosmg@gmail.com>
2026-04-02 23:09:50 -0300

    Create bebidas.js

---
+++ bebidas.js
@@ -0,0 +1,116 @@
+// ==========================
+// BEBIDAS
+// ==========================
+const BEBIDAS = [
+
+    {
+        id: 101,
+        nome: "Coca-Cola Lata 350ml",
+        descricao: "",
+        preco: 5.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "bebida",
+        disponivel: true
+    },
+
+    {
+        id: 102,
+        nome: "Coca-Cola Zero Lata",
+        descricao: "",
+        preco: 5.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "bebida",
+        disponivel: true
+    },
+
+    {
+        id: 103,
+        nome: "Fanta Laranja Lata",
+        descricao: "",
+        preco: 5.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "bebida",
+        disponivel: true
+    },
+
+    {
+        id: 104,
+        nome: "Guaraná Antártica Lata",
+        descricao: "",
+        preco: 5.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "bebida",
+        disponivel: true
+    },
+
+    {
+        id: 201,
+        nome: "Pepsi 1L",
+        descricao: "",
+        preco: 7.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "bebida",
+        disponivel: true
+    },
+
+    {
+        id: 202,
+        nome: "Sukita 1L",
+        descricao: "",
+        preco: 7.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "bebida",
+        disponivel: true
+    },
+
+    {
+        id: 203,
+        nome: "Guaraná Antártica 1L",
+        descricao: "",
+        preco: 7.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "bebida",
+        disponivel: true
+    },
+
+    {
+        id: 301,
+        nome: "Coca-Cola 2L",
+        descricao: "",
+        preco: 10.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "bebida",
+        disponivel: true
+    },
+
+    {
+        id: 302,
+        nome: "Coca-Cola Zero 2L",
+        descricao: "",
+        preco: 10.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "bebida",
+        disponivel: true
+    },
+
+    {
+        id: 303,
+        nome: "Fanta Laranja 2L",
+        descricao: "",
+        preco: 10.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "bebida",
+        disponivel: true
+    },
+
+    {
+        id: 304,
+        nome: "Guaraná Antártica 2L",
+        descricao: "",
+        preco: 10.00,
+        imagem: "https://via.placeholder.com/300",
+        tipo: "bebida",
+        disponivel: true
+    }
+
+];
