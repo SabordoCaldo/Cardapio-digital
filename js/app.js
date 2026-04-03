904f3e782adb98d48c26223c50064a0aee19ce61
Sabor do caldo <sabordocaldo.passosmg@gmail.com>
2026-04-03 19:32:30 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 function trocarTela(id) {
   document.querySelectorAll(".tela").forEach(t => t.classList.remove("ativa"));
-  document.getElementById(id).classList.add("ativa");
+
+  const tela = document.getElementById(id);
+  if (!tela) return;
+
+  tela.classList.add("ativa");
 
   if (id === "inicio") carregarInicio();
   if (id === "carrinho") carregarCarrinho();
+
   if (id === "perfil") {
-  carregarHistorico();
-  mostrarDadosCliente();
+    if (typeof carregarHistorico === "function") {
+      carregarHistorico();
+    }
+
+    if (typeof mostrarDadosCliente === "function") {
+      mostrarDadosCliente();
+    }
+  }
 }
 
 carregarInicio();
