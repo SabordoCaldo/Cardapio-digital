58e643d39fd364131e5469c220ad320ff509c3ea
Sabor do caldo <sabordocaldo.passosmg@gmail.com>
2026-04-04 07:04:39 -0300

    Update and rename js/perfil.js to js/perfil/dados.js

---
+++ js/perfil/dados.js
@@ -0,0 +1,16 @@
+function editarPerfil() {
+  trocarTela("dados");
+
+  document.getElementById("nome").value = localStorage.getItem("nome") || "";
+  document.getElementById("endereco").value = localStorage.getItem("endereco") || "";
+  document.getElementById("telefone").value = localStorage.getItem("telefone") || "";
+}
+
+function salvar() {
+  localStorage.setItem("nome", nome.value);
+  localStorage.setItem("endereco", endereco.value);
+  localStorage.setItem("telefone", telefone.value);
+
+  alert("Dados salvos!");
+  trocarTela("perfil");
+}
